d895d6994f1ac837fc44395132b5282f84a52ed2
Serdesiyon <serdesiyon.tsegaye@aastustudent.edu.et>
2023-12-05 12:35:44 +0300

    snake_case correction on student database

--- week2/studentDatabase.py
+++ week2/studentDatabase.py
@@ -1,33 +1,35 @@
-studentInfo = {
+student_info = {
 
 }
 
 
-def addStudents (name, age, grade,sex,networth):
-    studentInfo[name] = {  
+def add_students(name, age, grade, sex, networth):
+    student_info[name] = {
         "age": age,
         "grade": grade,
         "sex": sex,
         "networth": networth
     }
 
-def removeStudents(name):
-    del studentInfo[name]
-      
-def viewStudents(name):
-    print(studentInfo[name])
+
+def remove_students(name):
+    del student_info[name]
 
 
-addStudents("Musk",48, "A", "Male" , 205) 
-addStudents("Zuckerberg",38, "A", "Male" ,102 ) 
-addStudents("Nadella",56, "A", "Male" , 2 ) 
-addStudents("Gates",64, "A", "Male", 110 ) 
-addStudents("Bezos",62, "A", "Male" , 159 ) 
-addStudents("Lovelace",120, "A", "Female", 0.5 ) 
-addStudents("Altman",120, "A", "Male", 4 ) 
+def view_students(name):
+    print(student_info[name])
 
-viewStudents("Bezos")
 
-removeStudents("Altman")
+add_students("Musk", 48, "A", "Male", 205)
+add_students("Zuckerberg", 38, "A", "Male", 102)
+add_students("Nadella", 56, "A", "Male", 2)
+add_students("Gates", 64, "A", "Male", 110)
+add_students("Bezos", 62, "A", "Male", 159)
+add_students("Lovelace", 120, "A", "Female", 0.5)
+add_students("Altman", 120, "A", "Male", 4)
 
-print(studentInfo)
+view_students("Bezos")
+
+remove_students("Altman")
+
+print(student_info)
